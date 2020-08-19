c78d7a6453667a0649fcdefefd3c15e2619c639a
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2020-08-18 20:00:19 -0600

    terminando seccion de modulos

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,25 @@
+import { RouterModule } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+const routes = [
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
