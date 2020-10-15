20fc9b2f7c6090e8ec3115520c765bc3d35b4089
Ernesto Cardenas Llanes <ernesto.llanes@neoris.com>
2020-10-15 13:15:05 -0600

    seteando el selector de temas

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -3,14 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-account-settings',
   templateUrl: './account-settings.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AccountSettingsComponent implements OnInit {
+  linkTheme = document.querySelector('#theme');
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeTheme(theme) {
+    const urlTheme = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute('href', urlTheme);
+    localStorage.setItem('theme', urlTheme);
   }
-
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PagesComponent implements OnInit {
+  linkTheme = document.querySelector('#theme');
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.linkTheme.setAttribute('href', './assets/css/colors/default-dark');
+    const urlTheme = localStorage.getItem('theme');
+    this.linkTheme.setAttribute('href', urlTheme);
   }
-
 }
