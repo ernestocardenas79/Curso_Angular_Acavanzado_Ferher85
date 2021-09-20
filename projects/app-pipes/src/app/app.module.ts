72a7c9c7db507bf4a89de8e5c490d58ae51e60ee
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2021-09-19 21:29:26 -0600

    Inciando aplicacion de pipes

---
+++ projects/app-pipes/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-router.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRouterModule, SharedModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
