330f1644bfa5309ce45cf9ade3f04a13f7d66e20
Ernesto Cardenas <ernesto.cardenas@Chubb.com>
2021-09-28 09:56:55 -0500

    Instalando angular material en heroesApp

--- projects/heroes-app/src/app/app.module.ts
+++ projects/heroes-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
