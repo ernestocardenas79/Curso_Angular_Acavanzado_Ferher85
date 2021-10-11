459f1fe41f2f039db0c308b295d496a3ec383b87
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2021-10-10 22:07:33 -0600

    Creando autenticacion para login

--- projects/heroes-app/src/app/auth/pages/login/login.component.ts
+++ projects/heroes-app/src/app/auth/pages/login/login.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'projects/heroes-app/src/environments/environment.prod';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private router: Router, private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    this.authService.login().subscribe((r) => {
+      if (r.id) {
+        this.router.navigate(['./heroes']);
+      }
+    });
   }
-
 }
